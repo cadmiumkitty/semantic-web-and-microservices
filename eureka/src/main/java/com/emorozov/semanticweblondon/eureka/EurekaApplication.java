a357e9c9bb506b530a28f8d3b56403c02ef8b698
Cadmium Kitty <emorozov@gmail.com>
2017-03-14 07:27:30 +0000

    Adds trades service stub, simplifies package structure by removing one level of hierarchy.

--- eureka/src/main/java/com/emorozov/semanticweblondon/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/emorozov/semanticweblondon/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.emorozov.semanticweblondon.examples.ms.eureka;
+package com.emorozov.semanticweblondon.eureka;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- zuul/src/main/java/com/emorozov/semanticweblondon/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/emorozov/semanticweblondon/zuul/ZuulApplication.java
@@ -1,4 +1,4 @@
-package com.emorozov.semanticweblondon.examples.ms.zuul;
+package com.emorozov.semanticweblondon.zuul;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
