9d46fa18d3f1a1804161db5f89b4c2459dcfaf90
Cadmium Kitty <emorozov@gmail.com>
2017-03-18 20:05:10 +0000

    Initial complete implementation of the orders microservice.

--- orders/src/main/java/com/emorozov/semanticweblondon/orders/OrdersController.java
+++ orders/src/main/java/com/emorozov/semanticweblondon/orders/OrdersController.java
@@ -2,16 +2,17 @@ package com.emorozov.semanticweblondon.orders;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
 import java.io.ByteArrayOutputStream;
 
 @Slf4j
@@ -28,29 +29,37 @@ public class OrdersController {
 
         log.info("Loading model...");
 
-        model = ModelFactory.createDefaultModel();
-        model.read("data/orders.ttl", Lang.TURTLE.getName());
+        this.model = ModelFactory.createDefaultModel();
+        this.model.read("data/orders.ttl", Lang.TURTLE.getName());
 
         log.info("Done.");
     }
 
     @RequestMapping(path = "/", method = RequestMethod.GET, produces = "application/json")
-    public String getTrades(HttpServletRequest request) throws Exception {
+    public void getTrades(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             RDFDataMgr.write(baos, model, Lang.JSONLD);
-            return baos.toString("UTF-8");
+            response.getWriter().write(baos.toString("UTF-8"));
+            response.setStatus(200);
         }
     }
 
-    @RequestMapping(path = "/{orderId}", method = RequestMethod.GET, produces = "application/json")
-    public String getTrade(HttpServletRequest request, String orderId) {
+    @RequestMapping(path = "/{orderid}", method = RequestMethod.GET, produces = "application/json")
+    public void getTrade(@PathVariable("orderid") String orderid, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        String requestUrl = request.getRequestURL().toString();
+        String url = String.format("http://localhost:50400/orders/%s", orderid);
+        StmtIterator iterator = model.listStatements(model.getResource(url), null, (RDFNode)null);
 
-        log.info("Request URL: {}", requestUrl);
+        Model orderModel = ModelFactory.createDefaultModel();
+        while (iterator.hasNext()) {
+            orderModel.add(iterator.nextStatement());
+        }
 
-        Resource resourceModel = model.getResource(requestUrl);
-
-        return resourceModel.toString();
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            RDFDataMgr.write(baos, orderModel, Lang.JSONLD);
+            response.getWriter().write(baos.toString("UTF-8"));
+            response.setStatus(200);
+        }
     }
 }
